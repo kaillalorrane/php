dc900009a1ade1a42d50817c9888dcf216f34653
Kailla <145464372+kaillalorrane@users.noreply.github.com>
2023-10-17 11:20:12 -0300

    aula-17-10-2023

---
+++ aula01/exp02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Livros-citações</title>
+</head>
+<body>
+    <?php
+   /* echo ' Passei a maior parte da vida protegendo meu coração. Protegi tão bem que me comportava como se não tivesse um. Mesmo agora, é uma coisa mequetrefe, comida de traças e escabrosa. Mas é seu.<br>';
+   echo ' Acha que só porque sou mulher sou incapaz de tomar minhas próprias decisões? <br>';
+    
+   echo' Não importa o quanto as coisas se quebrem, elas podem ser consertadas. E podem se tornar ainda melhores do que eram antes <br> ';
+   
+   echo ' É assim que descobrimos que estamos fazendo o que estávamos destinados a fazer: quando nunca nos entediamos e não queremos fazer nenhuma outra coisa. <br>';
+  
+   print_r ("livros: Rainha do nada, Bridgertons, Uma surpresa na primavera e a Garota do lago");
+   
+*/
+ ?>
+</body>
+</html>
---
+++ aula01/index.php
@@ -0,0 +1,18 @@
+<?php
+echo 'Ele é uma faísca, e você é altamente inflamável. ';
+echo'<br>';
+echo '<hr>';
+?>
+
+
+<?= " Estávamos nos aproximmando a 100 quilômetros por hora.E de repente eu tive que frear...Ele bateu de frente e eu me quebrei em mil pedaços"?>
+<?php
+
+// echo "comentario de uma linha";
+
+/*comentario de varias linhas
+nova linha agora
+nova linha agora
+*/
+
+?>
