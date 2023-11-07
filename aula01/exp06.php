c2c70395603c220cb198449465560efee98ae21c
Kailla <145464372+kaillalorrane@users.noreply.github.com>
2023-11-07 08:49:01 -0300

    Add files via upload

---
+++ aula01/exp06.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!";
+?>
+
+ <?php
+$txt = "W3Schools.com";
+echo "I love " . $txt . "! ";
+?>
+
+<?php
+$x = 5;
+$y = 4;
+echo $x + $y;
+?>
+</body>
+</html>
---
+++ aula01/exp07.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "<h2>PHP is fun!</h2>";
+    echo "Hello world! <br> ";
+    echo " I'm about to learn PHP <br> ";
+    print "This ". "string ". "was ". "made ". "with multiple parameters.";
+    ?>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+?>
+
+
+<?php
+print "<h2>PHP is Fun! </h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?>
+</body>
+</html>
