581d37f16d800a58431f028653a166d49c651e17
Kailla <145464372+kaillalorrane@users.noreply.github.com>
2023-11-07 11:36:44 -0300

    php-css

---
+++ aula01/aula02/exp02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    <h1> livros</h1>
+    <p> books</p>
+    <h2> libri </h2>
+    <p id="p2"> livres</p>
+
+    <p class= "style"> bøker</p>
+    <p class= "style">bøker</p>
+    <h1 class= "style">bøker</h1>
+</body>
+</html>
