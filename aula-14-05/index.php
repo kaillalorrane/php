89b0dde45ff908a016e412b4ad9da08755aa3ba9
Kailla <145464372+kaillalorrane@users.noreply.github.com>
2024-05-14 08:16:00 -0300

    Add files via upload

---
+++ aula-14-05/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+<h1> array_lacos </h1>
+<h2> exemplo 01</h2>
+<pre>  
+<?php
+
+$a1=array("antonio", "luis", "jose", "joao");
+echo $a1[0];
+
+
+?>
+</pre>
+
+</body>
+</html>
+
+
+
+
+
